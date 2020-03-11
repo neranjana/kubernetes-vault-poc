891e2ab2f9943daac391c04b37ef84ab21bb0730
Neranjana Karunaratne <neranjana@Neranjanas-MacBook-Pro.local>
2020-03-11 16:37:48 +1100

    Running docker on OpenShift

--- sampleserviceone/dockerrunsampleserviceone.sh
+++ sampleserviceone/dockerrunsampleserviceone.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-docker run -p 8082:8081 -it --rm --name running-sample-service-one sampleserviceone:latest
+# docker run -p 8081:8081 -it --rm --name running-sample-service-one sampleserviceone:latest --secrets.secret1='Captain America' --secrets.secret2='Hulk'
+docker run -p 8081:8081 -it --rm --name running-sample-service-one sampleserviceone:latest --spring.config.location=classpath:application.yml,/Users/neranjana/dev/kubernetes-vault-poc/sampleserviceone/secrets.properties
